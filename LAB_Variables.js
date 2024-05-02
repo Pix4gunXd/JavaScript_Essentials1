f57ca187fbd11be5548c7184802d9f81c5ece81f
Pix4gunXd <Caio.pompei@gmail.com>
2024-05-02 15:56:39 -0300

    Add Files

---
+++ LAB_Variables.js
@@ -0,0 +1,21 @@
+let nome = ["Maxwell Wright", "Raja Villarreal", "Helen Richards"]
+
+let nome1 = "Maxwell Wright";
+let nome2 = "Raja Villarreal";
+let nome3 = "Helen Richards";
+
+let phone = [" (0191) 719 6495", " 0866 398 2895", " 0800 1111"]
+
+let phone1 = "(0191) 719 6495";
+let phone2 = "0866 398 2895";
+let phone3 = "0800 1111";
+
+let email = [" Curabitur.egestas.nunc@nonummyac.co.uk", " posuere.vulputate@sed.com", " libero@convallis.edu"]
+
+let email1 = "Curabitur.egestas.nunc@nonummyac.co.uk";
+let email2 = "posuere.vulputate@sed.com";
+let email3 = "libero@convallis.edu";
+
+for (i = 0; i < 3; i++) {
+    console.log(nome[i] + phone[i] + email[i]);
+}
